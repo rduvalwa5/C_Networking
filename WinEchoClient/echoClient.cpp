2d83f709d198844694ba867a48ca06714d3634ac
rduvalwa5@hotmail.com <rduvalwa5@hotmail.com>
2018-10-11 23:18:21 -0700

    Windows server work

--- WinEchoClient/echoClient.cpp
+++ WinEchoClient/echoClient.cpp
@@ -1,131 +1,77 @@
-/*
+ /*
  * echoClient.cpp
  *
- *  Created on: Oct 5, 2018
- *      Author: RDuval
- */
-#define WIN32_LEAN_AND_MEAN
+ *  Created on: Oct 4, 2018
+ *      Author: rduvalwa2
+ *
+ * C:\Users\rduvalwa2\Cworkspace\WinEchoclient>tasklist /fi "imagename eq WinEchoServer.exe"
+ *
+ * Image Name                     PID Session Name        Session#    Mem Usage
+ * ========================= ======== ================ =========== ============
+ * WinEchoServer.exe             3848 Console                    1      2,204 K
+ *
+ *	C ECHO client example using sockets
+ *	Also, please not that the lib is named "ws2_32"
+ *
+ *	gcc -o echoClientWin.exe echoClient.o -lws2_32
+*/
+#include<stdio.h>	//printf
+#include<string.h>	//strlen
+//#include<sys/socket.h>	//socket
+//#include<arpa/inet.h>	//inet_addr
+#include<winsock2.h>
 
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-
-// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
-//#pragma comment (lib, "Ws2_32.lib")
-//#pragma comment (lib, "Mswsock.lib")
-//#pragma comment (lib, "AdvApi32.lib")
-
-
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
-
-int __cdecl main(int argc, char **argv)
+int main(int argc , char *argv[])
 {
-    WSADATA wsaData;
-    SOCKET ConnectSocket = INVALID_SOCKET;
-    struct addrinfo *result = NULL,
-                    *ptr = NULL,
-                    hints;
-    char *sendbuf = "this is a test";
-    char recvbuf[DEFAULT_BUFLEN];
-    int iResult;
-    int recvbuflen = DEFAULT_BUFLEN;
+	int sock;
+	struct sockaddr_in server;
+	char message[1000] , server_reply[2000];
 
-    // Validate the parameters
-    if (argc != 2) {
-        printf("usage: %s server-name\n", argv[0]);
-        return 1;
-    }
+	//Create socket
+	sock = socket(AF_INET , SOCK_STREAM , 0);
+	if (sock == -1)
+	{
+		printf("Could not create socket");
+	}
+	puts("Socket created");
 
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed with error: %d\n", iResult);
-        return 1;
-    }
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 8888 );
 
-    ZeroMemory( &hints, sizeof(hints) );
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
+	//Connect to remote server
+	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		perror("connect failed. Error");
+		return 1;
+	}
 
-    // Resolve the server address and port
-    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
-    if ( iResult != 0 ) {
-        printf("getaddrinfo failed with error: %d\n", iResult);
-        WSACleanup();
-        return 1;
-    }
+	puts("Connected\n");
 
-    // Attempt to connect to an address until one succeeds
-    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+	//keep communicating with server
+	while(1)
+	{
+		printf("Enter message : ");
+		scanf("%s" , message);
 
-        // Create a SOCKET for connecting to server
-        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
-            ptr->ai_protocol);
-        if (ConnectSocket == INVALID_SOCKET) {
-            printf("socket failed with error: %ld\n", WSAGetLastError());
-            WSACleanup();
-            return 1;
-        }
+		//Send some data
+		if( send(sock , message , strlen(message) , 0) < 0)
+		{
+			puts("Send failed");
+			return 1;
+		}
 
-        // Connect to server.
-        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-        if (iResult == SOCKET_ERROR) {
-            closesocket(ConnectSocket);
-            ConnectSocket = INVALID_SOCKET;
-            continue;
-        }
-        break;
-    }
+		//Receive a reply from the server
+		if( recv(sock , server_reply , 2000 , 0) < 0)
+		{
+			puts("recv failed");
+			break;
+		}
 
-    freeaddrinfo(result);
+		puts("Server reply :");
+		puts(server_reply);
+	}
 
-    if (ConnectSocket == INVALID_SOCKET) {
-        printf("Unable to connect to server!\n");
-        WSACleanup();
-        return 1;
-    }
-
-    // Send an initial buffer
-    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
-    if (iResult == SOCKET_ERROR) {
-        printf("send failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Bytes Sent: %ld\n", iResult);
-
-    // shutdown the connection since no more data will be sent
-    iResult = shutdown(ConnectSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        printf("shutdown failed with error: %d\n", WSAGetLastError());
-        closesocket(ConnectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Receive until the peer closes the connection
-    do {
-
-        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-        if ( iResult > 0 )
-            printf("Bytes received: %d\n", iResult);
-        else if ( iResult == 0 )
-            printf("Connection closed\n");
-        else
-            printf("recv failed with error: %d\n", WSAGetLastError());
-
-    } while( iResult > 0 );
-
-    // cleanup
-    closesocket(ConnectSocket);
-    WSACleanup();
-
-    return 0;
+//	close(sock);
+	return 0;
 }
---
+++ WinEchoServer/WinEchoServer.cpp
@@ -0,0 +1,91 @@
+/*
+ * https://www.binarytides.com/code-tcp-socket-server-winsock/
+ *   Bind socket to port 8888 on localhost
+ *
+ *   In Eclipse go to project > properties > MingW Linker C++ > Libraries
+ *   add ws2_32
+ *   this puts this library in the linker path
+ *   in winsock.h
+ *    (gs)etsockopts exported from the WinSock 2 lib, ws2_32.dll.
+ *    Refer ws2tcpip.h.
+ *
+ *    g++ -o WinEchoServer.exe echoServer.o -lws2_32
+ *
+ * C:\Users\rduvalwa2\Cworkspace\WinEchoclient>tasklist /fi "imagename eq WinEchoServer.exe"
+ *
+ * Image Name                     PID Session Name        Session#    Mem Usage
+ * ========================= ======== ================ =========== ============
+ * WinEchoServer.exe             3848 Console                    1      2,204 K
+ *
+ */
+#include<io.h>
+#include<stdio.h>
+#include<winsock2.h>
+
+//#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+int main(int argc , char *argv[])
+{
+    WSADATA wsa;
+    SOCKET s , new_socket;
+    struct sockaddr_in server , client;
+    int c;
+    char * message;
+
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d",WSAGetLastError());
+        return 1;
+    }
+
+    printf("Initialised.\n");
+
+    //Create a socket
+    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d" , WSAGetLastError());
+    }
+
+    printf("Socket created.\n");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+
+    //Bind
+    if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code : %d" , WSAGetLastError());
+    }
+
+    puts("Bind done");
+
+    //Listen to incoming connections
+    listen(s , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+
+    c = sizeof(struct sockaddr_in);
+    new_socket = accept(s , (struct sockaddr *)&client, &c);
+    if (new_socket == INVALID_SOCKET)
+    {
+        printf("accept failed with error code : %d" , WSAGetLastError());
+    }
+
+    puts("Connection accepted");
+
+    //Reply to client
+    message = "Hello Client , I have received your connection. But I have to go now, bye\n";
+    send(new_socket , message , strlen(message) , 0);
+
+    getchar();
+
+    closesocket(s);
+    WSACleanup();
+
+    return 0;
+}
+
